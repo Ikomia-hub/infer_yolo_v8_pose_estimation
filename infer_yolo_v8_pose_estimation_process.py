99d17aa63de47aa759691e8377c812859a734ff7
Allan Kouidri <87222273+allankouidri@users.noreply.github.com>
2023-11-16 14:56:33 +0100

    patch version: fix model download with ultralytics v8.0.117

--- infer_yolo_v8_pose_estimation_process.py
+++ infer_yolo_v8_pose_estimation_process.py
@@ -215,7 +215,7 @@ class InferYoloV8PoseEstimationFactory(dataprocess.CTaskFactory):
         self.info.short_description = "Inference with YOLOv8 pose estimation models"
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Pose"
-        self.info.version = "1.0.1"
+        self.info.version = "1.0.2"
         self.info.icon_path = "icons/icon.png"
         self.info.authors = "Jocher, G., Chaurasia, A., & Qiu, J"
         self.info.article = "YOLO by Ultralytics"
